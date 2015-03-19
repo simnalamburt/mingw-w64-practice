0f3c88cc68a20e8fae39c5927806ccbb28681e61
Hyeon Kim <simnalamburt@gmail.com>
2015-03-20 05:02:03 +0900

    Cleaned codes

--- main.c
+++ main.c
@@ -1,73 +1,68 @@
 #include <windows.h>
-#include <GL/GL.h>
+#include <gl/gl.h>
 
-#pragma comment (lib, "opengl32.lib")
 
-LRESULT CALLBACK WndProc(HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam);
+LRESULT CALLBACK event_handler(HWND, UINT, WPARAM, LPARAM);
 
-int WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine, int nShowCmd )
-{
-  MSG msg          = {0};
-  WNDCLASSW wc     = {0};
-  wc.lpfnWndProc   = WndProc;
-  wc.hInstance     = hInstance;
-  wc.hbrBackground = (HBRUSH)(COLOR_BACKGROUND);
-  wc.lpszClassName = L"oglversionchecksample";
-  wc.style = CS_OWNDC;
-  if( !RegisterClassW(&wc) )
-    return 1;
-  CreateWindowW(wc.lpszClassName,L"openglversioncheck",WS_OVERLAPPEDWINDOW|WS_VISIBLE,0,0,640,480,0,0,hInstance,0);
+int WinMain(HINSTANCE instance, HINSTANCE _, LPSTR _cmd, int _show) {
+  WNDCLASSW wc = {};
+  wc.lpfnWndProc    = event_handler;
+  wc.hInstance      = instance;
+  wc.hbrBackground  = (HBRUSH)(COLOR_BACKGROUND);
+  wc.lpszClassName  = L"oglversionchecksample";
+  wc.style          = CS_OWNDC;
+  if (!RegisterClassW(&wc)) { return 1; }
+  CreateWindowW(wc.lpszClassName, L"openglversioncheck",
+    WS_OVERLAPPEDWINDOW | WS_VISIBLE, 0, 0, 640, 480, 0, 0, instance, 0);
 
-  while( GetMessage( &msg, NULL, 0, 0 ) > 0 )
-    DispatchMessage( &msg );
+  MSG msg = {};
+  while (GetMessage(&msg, NULL, 0, 0) > 0)
+    DispatchMessage(&msg);
 
   return 0;
 }
 
-LRESULT CALLBACK WndProc(HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam)
-{
-  switch(message)
-  {
-  case WM_CREATE:
-    {
-    PIXELFORMATDESCRIPTOR pfd =
-    {
-      sizeof(PIXELFORMATDESCRIPTOR),
-      1,
-      PFD_DRAW_TO_WINDOW | PFD_SUPPORT_OPENGL | PFD_DOUBLEBUFFER,    //Flags
-      PFD_TYPE_RGBA,            //The kind of framebuffer. RGBA or palette.
-      32,                        //Colordepth of the framebuffer.
-      0, 0, 0, 0, 0, 0,
-      0,
-      0,
-      0,
-      0, 0, 0, 0,
-      24,                        //Number of bits for the depthbuffer
-      8,                        //Number of bits for the stencilbuffer
-      0,                        //Number of Aux buffers in the framebuffer.
-      PFD_MAIN_PLANE,
-      0,
-      0, 0, 0
-    };
-
-    HDC ourWindowHandleToDeviceContext = GetDC(hWnd);
-
-    int  letWindowsChooseThisPixelFormat;
-    letWindowsChooseThisPixelFormat = ChoosePixelFormat(ourWindowHandleToDeviceContext, &pfd);
-    SetPixelFormat(ourWindowHandleToDeviceContext,letWindowsChooseThisPixelFormat, &pfd);
-
-    HGLRC ourOpenGLRenderingContext = wglCreateContext(ourWindowHandleToDeviceContext);
-    wglMakeCurrent (ourWindowHandleToDeviceContext, ourOpenGLRenderingContext);
-
-    MessageBoxA(0,(char*)glGetString(GL_VERSION), "OPENGL VERSION",0);
-
-    wglDeleteContext(ourOpenGLRenderingContext);
-    PostQuitMessage(0);
-    }
-    break;
-  default:
-    return DefWindowProc(hWnd, message, wParam, lParam);
+LRESULT CALLBACK event_handler(HWND window, UINT message, WPARAM w_param, LPARAM l_param) {
+  if (message != WM_CREATE) {
+    return DefWindowProc(window, message, w_param, l_param);
   }
-  return 0;
 
+  PIXELFORMATDESCRIPTOR fmt_desc = {
+    sizeof(PIXELFORMATDESCRIPTOR),
+    1,
+    PFD_DRAW_TO_WINDOW | PFD_SUPPORT_OPENGL | PFD_DOUBLEBUFFER,
+                        //Flags
+    PFD_TYPE_RGBA,      // The kind of framebuffer. RGBA or palette.
+    32,                 // Colordepth of the framebuffer.
+    0, 0, 0, 0, 0, 0,
+    0,
+    0,
+    0,
+    0, 0, 0, 0,
+    24,                 // Number of bits for the depthbuffer
+    8,                  // Number of bits for the stencilbuffer
+    0,                  // Number of Aux buffers in the framebuffer.
+    PFD_MAIN_PLANE,
+    0,
+    0, 0, 0
+  };
+
+  // Retrieve device context from window handle
+  HDC hdc = GetDC(window);
+
+  // Let OS choose their pixel format
+  int fmt;
+  fmt = ChoosePixelFormat(hdc, &fmt_desc);
+  SetPixelFormat(hdc, fmt, &fmt_desc);
+
+  // Create OpenGL rendering context
+  HGLRC ctxt = wglCreateContext(hdc);
+  wglMakeCurrent(hdc, ctxt);
+
+  MessageBoxA(0, (char*)glGetString(GL_VERSION), "OPENGL VERSION", 0);
+
+  wglDeleteContext(ctxt);
+  PostQuitMessage(0);
+
+  return 0;
 }
